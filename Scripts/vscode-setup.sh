fb48fd9a3df5360a25bc15590252c6f8910268db
markbac <109081587+markbac@users.noreply.github.com>
2023-06-23 10:24:38 +0100

    Update vscode-setup.sh

--- Scripts/vscode-setup.sh
+++ Scripts/vscode-setup.sh
@@ -8,6 +8,7 @@ MACH=$(uname -m)
 
 declare -a vscodeextensions=(
     42Crunch.vscode-openapi
+    aaron-bond.better-comments
     4ops.terraform
     aaron-bond.better-comments
     alefragnani.project-manager
@@ -15,7 +16,9 @@ declare -a vscodeextensions=(
     anweber.vscode-httpyac
     aprilandjan.ascii-tree-generator
     asyncapi.asyncapi-preview
+    AykutSarac.jsoncrack-vscode
     batisteo.vscode-django
+    bierner.markdown-mermaid
     BitBelt.converttoasciiart
     bmlsolutions.dotuml
     bpruitt-goddard.mermaid-markdown-syntax-highlighting
@@ -23,9 +26,12 @@ declare -a vscodeextensions=(
     chrmarti.regex
     daiyy.quick-html-previewer
     DavidAnson.vscode-markdownlint
+    derek-nextty.openapi-viewer
     docsmsft.docs-yaml
     donjayamanne.python-environment-manager
     donjayamanne.python-extension-pack
+	DotJoshJohnson.xml
+    DougFinke.vscode-pandoc
     eamodio.gitlens
     esbenp.prettier-vscode
     fernandoescolar.vscode-solution-explorer
@@ -39,13 +45,14 @@ declare -a vscodeextensions=(
     kamikillerto.vscode-colorize
     KevinRose.vsc-python-indent
     maattdd.gitless
+    Markwhen.markwhen
     marp-team.marp-vscode
+    MartinPicard.vscode-openapi
     mermade.openapi-lint
     mirone.milkdown
+    mkloubert.vscode-kanban
     mohsen1.prettify-json
-    ms-azuretools.vscode-docker
-    ms-edgedevtools.vscode-edge-devtools
-    ms-python.isort
+    ms-dotnettools.csharp
     ms-python.python
  #   ms-python.vscode-pylance
     ms-toolsai.jupyter-keymap
@@ -54,14 +61,20 @@ declare -a vscodeextensions=(
     ms-vsliveshare.vsliveshare
     ms-vsliveshare.vsliveshare-audio
     njpwerner.autodocstring
+    nopeslide.vscode-drawio-plugin-mermaid
+    philosowaffle.openapi-designer
     pomdtr.excalidraw-editor
     rangav.vscode-thunder-client
     redhat.vscode-xml
     redhat.vscode-yaml
+	RoeySagie.markdown-sort
     shd101wyy.markdown-preview-enhanced
+    simonguo.vscode-markdown-table-sort
     stoplight.spectral
     streetsidesoftware.code-spell-checker
     tberman.json-schema-validator
+    tehpeng.diagramspreviewer
+    telesoho.vscode-markdown-paste-image
     tintinweb.graphviz-interactive-preview
     tomoki1207.pdf
     tonybaloney.vscode-pets
@@ -69,6 +82,7 @@ declare -a vscodeextensions=(
     VisualStudioExptTeam.vscodeintellicode
     VolcanicBytes.xsd-navigator
     voldemortensen.rainbow-tags
+    vstirbu.vscode-mermaid-preview
     vscode-icons-team.vscode-icons
     waleedashraf.asyncapi-validator-vscode
     wayou.vscode-todo-highlight
@@ -77,6 +91,7 @@ declare -a vscodeextensions=(
     yzane.markdown-pdf
     yzhang.markdown-all-in-one
     zaaack.markdown-editor
+    zoellner.openapi-preview
 )
 
 echo "#####"
