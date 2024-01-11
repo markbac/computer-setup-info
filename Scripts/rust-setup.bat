540ebca4aa23a5e591d625fe2ca0948d2125758e
markbac <109081587+markbac@users.noreply.github.com>
2024-01-11 11:40:25 +0000

    Update rust-setup.bat

--- Scripts/rust-setup.bat
+++ Scripts/rust-setup.bat
@@ -1,77 +1,52 @@
-#!/bin/bash
-#ubuntu
-#export HISTIGNORE='*sudo -S*'
+@echo off
+setlocal enabledelayedexpansion
 
-OS=$(uname -s)
-REV=$(uname -r)
-MACH=$(uname -m)
-
-declare -a rustpackages=(
-    du-dust # Dust provides a nice alternative to the Linux du command. The dust command allows users to quickly check disk space usage on any Linux system and see the results in an intuitive graph-based format
-    tokei  # Tokei is a program that displays statistics about your code
-    bottom # A customizable cross-platform graphical process/system monitor for the terminal. 
-    gitui # GitUI provides you with the comfort of a git GUI but right in your terminal
-    webget # get files from websites
-    macchina # Fast, minimal and customizable system information frontend.
-    eureka # is a CLI tool that allows you to quickly write down an idea using your preferred editor, and then have the idea committed and pushed to your idea git repository.
-    lsd # An ls with a lot of pretty colors and awesome icons
-    bat # another version of cat
-    #Cherrybomb # A CLI tool that helps avoid undefined user behaviour by validating your API descriptions, to make sure key parts are not missing or vague.
-    #openapi-fuzzer # Based on OpenAPI specification, openapi-fuzzer provides random data as inputs to the API endpoints in order to find bugs.
-    bandwhich # Terminal bandwidth utilization tool
-    broot # is another rust tool, through which you can navigate directories.
-    dua # "View disk space usage and delete unwanted data, fast."
-    mdbook # Create books from markdown files. 
+set "rustpackages=(
+    du-dust
+    tokei
+    bottom
+    gitui
+    webget
+    macchina
+    eureka
+    lsd
+    bat
+    bandwhich
+    broot
+    dua
+    mdbook
     mdbook-pdf
     mdbook-plantuml
     mdbook-codeblocks
     mdbook-mermaid
-    lfs # A Linux utility to get information on filesystems; like df but better
-    mdcat # Fancy cat for Markdown
-    procs # A modern replacement for ps written in Rust
-    topgrade # Upgrade all of your tools
+    lfs
+    mdcat
+    procs
+    topgrade
     cargo-update
-    trippy # A network diagnostic tool, inspired by mtr.
-    #jex # is an interactive tool for querying json files
+    trippy
     onefetch
-    rust-kanban # Kanban App for the terminal written in rust. This kanban app will allow the user to be more productive by prioritizing tasks and achieving goals faster
-    kanorg # KanOrg allows you easily organize all your tasks from the terminal. https://lib.rs/crates/kanorg
-    ttdl # terminal todo list
-    pask # a personal task manager for the command line.
-    task-picker #Task Picker helps you to keep an overview about tasks from different task trackers, like GitHub or CalDAV.
-    todo-bin # todo cli
-    rink # unit conversion
-    alacritty # Terminal
-    rh # http cli client
-    # notes-cli # A simple note taking CLI https://github.com/wez470/notes-cli
+    rust-kanban
+    kanorg
+    ttdl
+    pask
+    task-picker
+    todo-bin
+    rink
+    alacritty
+    rh
+)"
+
+echo #####
+echo Install rust packages
+for %%i in (%rustpackages%) do (
+    echo #####
+    echo.
+    echo Installing %%i
+    cargo install %%i
+    echo.
+    echo #####
+    echo.
 )
 
-echo "Install rust"
-curl https://sh.rustup.rs -sSf | sh
-
-source $HOME/.cargo/env
-# add the following to you ~/.bashrc
-PATH=$PATH:/home/mbacon/.cargo/bin
-
-cargo install cargo-update
-
-echo "#####"
-echo "Install rust packages"
-for rustpackage in "${rustpackages[@]}"; do
-    echo "#####"
-    echo ""
-    echo "Installing ${rustpackage}"
-    cargo install ${rustpackage}
-    echo ""
-    echo "#####"
-    echo ""
-done
-
-
-echo "#####"
-echo ""
-echo "Installing xplr"
-cargo install --locked --force xplr
-echo ""
-echo "#####"
-echo ""
+endlocal
