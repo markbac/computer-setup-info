b7daa37a1e2a6f930bc243c5bb888857e3b9d789
markbac <109081587+markbac@users.noreply.github.com>
2024-01-18 15:09:12 +0000

    Update scoop-setup.bat

--- Windows/scoop-setup.bat
+++ Windows/scoop-setup.bat
@@ -9,7 +9,7 @@ powershell -command "scoop --version" > nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     ECHO Scoop is not installed. Installing Scoop...
     powershell -command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
-    powershell -command "IInvoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression"
+    powershell -command "Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression"
 ) ELSE (
     ECHO Scoop is already installed.
 )
