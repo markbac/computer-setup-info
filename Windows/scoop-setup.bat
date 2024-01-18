3e14e6bc4bb9efca4c21a45abb210a5c8c09e666
markbac <109081587+markbac@users.noreply.github.com>
2024-01-18 14:20:36 +0000

    Update scoop-setup.bat

--- Windows/scoop-setup.bat
+++ Windows/scoop-setup.bat
@@ -1,1 +1,33 @@
+@echo off
+SETLOCAL
+
+:: Define a list of Scoop packages to install
+SET PACKAGES=topgrade git vim
+
+:: Check if Scoop is installed by trying to get its version
+powershell -command "scoop --version" > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Scoop is not installed. Installing Scoop...
+    powershell -command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser"
+    powershell -command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')"
+) ELSE (
+    ECHO Scoop is already installed.
+)
+
+:: Check again if Scoop is installed (in case installation failed)
+powershell -command "scoop --version" > nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Failed to install Scoop. Exiting script.
+    EXIT /B 1
+)
+
+:: Install Scoop packages
+ECHO Installing Scoop packages...
+FOR %%P IN (%PACKAGES%) DO (
+    ECHO Installing %%P...
+    scoop install %%P
+)
+
+ECHO Installation complete.
+PAUSE
 
